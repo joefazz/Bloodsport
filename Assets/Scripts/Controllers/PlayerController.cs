052f9c6f21217c7d664de01ac7e9f25e6be07dfa
JoeFazz <joseph@fazzino.net>
2021-02-22 22:46:19 +0000

    Some work

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -4,100 +4,112 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Android;
+using UnityEngine.VFX;
 
 namespace Controllers
 {
-    public class PlayerController : MonoBehaviour
-    {
-        [SerializeField] private Camera camera;
+	public class PlayerController : MonoBehaviour
+	{
+		[SerializeField]
+		private Camera camera;
 
-        [SerializeField] private CharacterController characterController;
+		[SerializeField]
+		private CharacterController characterController;
 
-        [SerializeField] private float health;
-        
-        [SerializeField] private Vector2 sensitivity;
+		[SerializeField]
+		private float health;
 
-        [SerializeField] private float playerSpeed = 1;
+		[SerializeField]
+		private Vector2 sensitivity;
 
-        private Vector2 moveDirection;
-        private float lookX;
-        private float lookY;
+		[SerializeField]
+		private float playerSpeed = 1;
 
-        private float minY = -60f;
-        private float maxY = 60f;
-        private int layerMask;
-        
-        private UIEventDispatcher uiEventDispatcher;
+		private Vector2 moveDirection;
 
-        public float Health => health;
+		private float lookX;
 
+		private float lookY;
 
-        public void InitDependencies(UIEventDispatcher uiEventDispatcher)
-        {
-            this.uiEventDispatcher = uiEventDispatcher;
-        }
-        
-        private void Start()
-        {
-            this.layerMask = 1 << 8;
-        }
+		private float minY = -60f;
 
-        private void Update()
-        {
-            if (health > 0)
-            {
-                Move();
-                Rotate();
-            }
-            else
-            {
-                uiEventDispatcher.PlayerKilled();
-            }
-        }
+		private float maxY = 60f;
 
-        private void Rotate()
-        {
-            lookY = Mathf.Clamp(lookY, minY, maxY);
-            transform.localEulerAngles = new Vector3(lookY, lookX, 0);
-        }
+		private int layerMask;
 
-        private void Move()
-        {
-            Vector3 movement =
-                (transform.forward * this.moveDirection.y + transform.right * this.moveDirection.x)
-                * playerSpeed;
+		private UIEventDispatcher uiEventDispatcher;
 
-            characterController.Move(movement);
-        }
+		public float Health => health;
 
-        public void OnFire(InputAction.CallbackContext ctx)
-        {
-            if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
-            {
-                Debug.Log("Hit thing");
-                hit.collider.gameObject.GetComponent<EnemyAI>().Damage();
-            }
-        }
+		public void InitDependencies(UIEventDispatcher uiEventDispatcher)
+		{
+			this.uiEventDispatcher = uiEventDispatcher;
+		}
 
-        public void OnMove(InputAction.CallbackContext ctx)
-        {
-            this.moveDirection = ctx.ReadValue<Vector2>();
-        }
+		private void Start()
+		{
+			this.layerMask = 1 << 8;
+		}
 
-        public void OnLook(InputAction.CallbackContext ctx)
-        {
-            this.lookX += ctx.ReadValue<Vector2>().x * sensitivity.x;
-            this.lookY -= ctx.ReadValue<Vector2>().y * sensitivity.y;
-        }
+		private void Update()
+		{
+			if (health > 0)
+			{
+				Move();
+				Rotate();
+			}
+			else
+			{
+				uiEventDispatcher.PlayerKilled();
+			}
+		}
 
-        private void OnCollisionEnter(Collision other)
-        {
-            Damage();
-        }
+		private void Rotate()
+		{
+			lookY = Mathf.Clamp(lookY, minY, maxY);
+			transform.localEulerAngles = new Vector3(lookY, lookX, 0);
+		}
 
-        public void Damage()
-        {
-            health -= 5;
-        }
-    }
+		private void Move()
+		{
+			Vector3 movement =
+				(transform.forward * this.moveDirection.y + transform.right * this.moveDirection.x)
+				* playerSpeed;
+
+			characterController.Move(movement);
+		}
+
+		public void OnFire(InputAction.CallbackContext ctx)
+		{
+			if (!ctx.performed) return;
+
+			GetComponentInChildren<VisualEffect>().Play();
+			if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+			{
+				Debug.Log("Hit thing");
+				hit.collider.gameObject.GetComponent<EnemyAI>().Damage();
+			}
+		}
+
+		public void OnMove(InputAction.CallbackContext ctx)
+		{
+			this.moveDirection = ctx.ReadValue<Vector2>();
+		}
+
+		public void OnLook(InputAction.CallbackContext ctx)
+		{
+			this.lookX += ctx.ReadValue<Vector2>().x * sensitivity.x;
+			this.lookY -= ctx.ReadValue<Vector2>().y * sensitivity.y;
+		}
+
+		private void OnCollisionEnter(Collision other)
+		{
+			Damage();
+		}
+
+		public void Damage()
+		{
+			health -= 5;
+		}
+	}
 }
